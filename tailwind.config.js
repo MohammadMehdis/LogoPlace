6011e9917f841c4602646789a44cf7ca552c9330
unknown <hosiniiiimehdi7412020@gmail.com>
2024-07-06 20:08:32 +0330

    installation tailwind / and preliminary work

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { rgb } = require("color-convert");
+const { yellow } = require("color-name");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+      },
+    },
+  },
+  plugins: [],
+};
