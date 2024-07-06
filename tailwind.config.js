6a8e34f3f4852c361e45359d5b60393a4d273c29
unknown <hosiniiiimehdi7412020@gmail.com>
2024-07-06 20:35:07 +0330

    Added font and colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
-const { rgb } = require("color-convert");
-const { yellow } = require("color-name");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
+    colors: {
+      gray: {
+        100: "#717171",
+        200: "#F1F1F2",
+        line: "#D3D4D6",
       },
-      fontFamily: {
+      black: {
+        100: "#0C0C0C",
+        200: "rgb(103,105,111,50%)",
+        footer: "#202020",
+      },
+      purple: {
+        100: "#6F4FF2",
+        200: "#866AFA",
+      },
+      while: {
+        100: "#FFFFFF",
+        200: "#F1F1F2",
       },
     },
+    fontFamily: {
+      Inter: "Inter",
+    },
+    // extend: {},
   },
   plugins: [],
 };
